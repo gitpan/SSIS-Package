d3b4eec3272d3a69cef30ac651a56de2a1f9c0fd
D Medved <CENSORED@hotmail.comb>
2013-06-05 15:54:13 +0000

    Import of DEDMEDVED/SSIS-Package-1.00_04 from CPAN.
    gitpan-cpan-distribution: SSIS-Package
    gitpan-cpan-version:      1.00_04
    gitpan-cpan-path:         DEDMEDVED/SSIS-Package-1.00_04.tar.gz
    gitpan-cpan-author:       DEDMEDVED
    gitpan-cpan-maturity:     developer

--- lib/SSIS/Package/ConnectionManager/ADONET.pm
+++ lib/SSIS/Package/ConnectionManager/ADONET.pm
@@ -8,20 +8,20 @@ use parent qw/SSIS::Package::ConnectionManager/;
 
 =head1 NAME
 
-SSIS::Package::ConnectionManager::ADO.NET - SSIS ADO.NET package connection manager by Ded MedVed
+SSIS::Package::ConnectionManager::ADONET - SSIS ADONET package connection manager by Ded MedVed
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 1 ;
 
 __DATA__
 
-1; # End of SSIS::Package::ConnectionManager::ADO.NET
+1; # End of SSIS::Package::ConnectionManager::ADONET
 
